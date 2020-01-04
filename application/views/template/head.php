3a56f6a8dcb1ccb0f1acbb48a63dfa2d3b6a304c
bruno <brunoantunes94@hotmail.com>
2020-01-04 20:34:20 -0300

    teste

--- application/views/template/head.php
+++ application/views/template/head.php
@@ -47,19 +47,27 @@
 		</div>
 		<!-- Load Facebook SDK for JavaScript -->
 		<div id="fb-root"></div>
-		<script>(function(d, s, id) {
+		<script>
+		window.fbAsyncInit = function() {
+		FB.init({
+		xfbml            : true,
+		version          : 'v5.0'
+		});
+		};
+
+		(function(d, s, id) {
 		var js, fjs = d.getElementsByTagName(s)[0];
 		if (d.getElementById(id)) return;
 		js = d.createElement(s); js.id = id;
-		js.src = 'https://connect.facebook.net/pt_BR/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
+		js.src = 'https://connect.facebook.net/pt_BR/sdk/xfbml.customerchat.js';
 		fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));</script>
 
 		<!-- Your customer chat code -->
 		<div class="fb-customerchat"
 		attribution=setup_tool
-		page_id="1156837047748369"
-		theme_color="#0084ff"
-		logged_in_greeting="Ola, Como posso ajudar você?"
-		logged_out_greeting="Ola, Como posso ajudar você?">
+		page_id="2379488575455820"
+		theme_color="#311607"
+		logged_in_greeting="Olá, Como posso ajudar você?"
+		logged_out_greeting="Olá, Como posso ajudar você?">
 		</div>
